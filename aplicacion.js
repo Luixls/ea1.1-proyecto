1eedd51cbe9d3fb1a2f5bf9b9364817364d07a23
Luixls <luisaraujo1998@gmail.com>
2024-02-09 21:15:22 -0400

    secciones + CRUD full
    bros a materias+profesores falta opciones de editar y eliminar

--- aplicacion.js
+++ aplicacion.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs');
-
+const seccionesRouter = require('./routes/secciones');
 const materiasRouter = require('./routes/materias');
 const profesoresRouter = require('./routes/profesores');
 
 app.use('/materias', materiasRouter);
 app.use('/profesores', profesoresRouter);
+app.use('/secciones', seccionesRouter);
 
 puerto = 3001
 app.listen(puerto, () => console.log('Servidor corriendo en puerto', puerto));
