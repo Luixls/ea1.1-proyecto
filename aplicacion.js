29b4c24da8fb94efe028cd2407404ee862ca1187
FN2184 <159496056+FN2184@users.noreply.github.com>
2024-02-10 01:47:47 -0400

    Eventos Completos
    Añadidas las funcionalidades de editar y eliminar eventos, creación del archivo ejs para observar eventos próximos

--- aplicacion.js
+++ aplicacion.js
@@ -6,11 +6,16 @@ const seccionesRouter = require('./routes/secciones');
 const materiasRouter = require('./routes/materias');
 const profesoresRouter = require('./routes/profesores');
 const eventosRouter = require('./routes/eventos');
+const EventoController = require('./controllers/EventoController');
 
 app.use('/materias', materiasRouter);
 app.use('/profesores', profesoresRouter);
 app.use('/secciones', seccionesRouter);
 app.use('/eventos', eventosRouter);
+
+app.get('/materia/eventos/:materiaId/:fechaInicio', EventoController.eventosPorSemana);
+
+app.get('/eventos/proximos/:fechaInicio', EventoController.eventosProximosPorProfesor);
 
 puerto = 3001
 app.listen(puerto, () => console.log('Servidor corriendo en puerto', puerto));
