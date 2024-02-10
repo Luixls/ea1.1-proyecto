080da85cae4430550a2b5866cda03a93c5a4e732
FN2184 <159496056+FN2184@users.noreply.github.com>
2024-02-10 09:26:28 -0400

    Código faltante
    Había subido lo que no era

--- routes/eventos.js
+++ routes/eventos.js
@@ -5,5 +5,7 @@ const EventoController = require('../controllers/EventoController');
 // Rutas para eventos
 router.post('/agregar', EventoController.agregar);
 router.get('/listar', EventoController.listar);
+router.put('/editar/:id', EventoController.editar);
+router.delete('/eliminar/:id', EventoController.eliminar);
 
 module.exports = router;
